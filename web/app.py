2cd2cdbc3dd4cd7b80bee35d13ad0ebb0ca4b401
James Addison <james@reciperadar.com>
2019-10-14 06:46:30 -0700

    Update variable name

--- web/app.py
+++ web/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def root():
-    url = request.args.getlist('products[]')
-    if not url:
+    products = request.args.getlist('products[]')
+    if not products:
         return abort(400)
 
-    return jsonify({})
+    return jsonify(products)
